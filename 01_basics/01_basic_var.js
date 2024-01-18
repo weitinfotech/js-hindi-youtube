31f9871706a0a4a343b6d0bbdfa59fc056c3aaac
dipesh swami <130590644+weitinfotech@users.noreply.github.com>
2024-01-18 16:58:52 +0000

    basicdatatype

---
+++ 01_basics/01_basic_var.js
@@ -0,0 +1,8 @@
+const accountId= "abc"
+let a = "dipesh@gmail.com"
+var b= "21212"
+
+a="htllo@gamil.com"
+b="23455"
+
+console.table([accountId,a,b])
---
+++ 01_basics/01_datatyep.js
@@ -0,0 +1,12 @@
+//number
+//bigint
+//stringo
+//boolean
+//null => standalone vale
+//undefined
+//symbol
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
